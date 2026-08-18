188ee1537a57cbb1468fade1254b706a07d8ffdc
mjusui <mjusui@gmail.com>
2026-08-18 19:12:59 +0000

    fix

--- sample_extension/background.js
+++ sample_extension/background.js
@@ -1,7 +1,8 @@
+// Background service worker
 console.log('Background service worker running');
 
-chrome.action.onClicked.addListener(async (tab) => {
-  await chrome.sidePanel.open({
-    tabId: tab.id
-  });
+// When the user clicks the extension's toolbar icon, open the side panel.
+chrome.action.onClicked.addListener(() => {
+  // Explicitly set the side panel page (optional – default_path already points here)
+  chrome.sidePanel.setOptions({path: 'side_panel.html'});
 });
