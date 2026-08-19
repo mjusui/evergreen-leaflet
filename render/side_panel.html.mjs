f1c4b919c33e566eb67938310ce04641ffffe7eb
mjusui <mjusui@gmail.com>
2026-08-19 14:58:02 +0000

    fix

--- render/side_panel.html.mjs
+++ render/side_panel.html.mjs
@@ -6,11 +6,11 @@ const scss=new Success()
   .classify('flex-col', 'display: flex', 'flex-direction: column', 'gap: 1rem')
   .classify('flex-row', 'display: flex', 'flex-direction: row', 'gap: 1rem', 'align-items: center')
   .classify('block', 'margin: 0.5rem', 'padding: 0.5rem', 'background-color: rgba(253,253,253,1.0)')
-  .classify('border', 'border-style: solid', 'border-width: 1px',
-     'border-color: rgba(55,55,55,1.0)', 'font-weight: bold' )
+  // .classify('border', 'border-style: solid', 'border-width: 1px',
+  //    'border-color: rgba(55,55,55,1.0)', 'font-weight: bold' )
   .classify('shadow', 'box-shadow: 0 0 1px 1px rgba(55,55,55,0.1)')
   .classify('margin-auto', 'margin: auto')
-  .classify('button', 'padding: 0.5rem' )
+  .classify('button', 'padding: 0.5rem', 'border-style: solid', 'border-width: 0px', 'border-radius: 1rem' )
   .classify('color1', 'background-color: rgba(31,181,115, 1.0)', 'color: rgba(253,253,253,1.0)' ) 
   .classify('modal-hidden', 'display: none')
   .classify('modal-display', 'display: block', 'position: fixed', 'top: 0',
