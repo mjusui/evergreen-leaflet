66eea72705f5f99da99df7acc4896aef540a59fc
mjusui <mjusui@gmail.com>
2026-08-19 04:05:00 +0000

    display top

--- render/render.mjs
+++ render/render.mjs
@@ -1,5 +1,6 @@
 import { main, } from '../bronze/debug/index.mjs';
 import { html, Success, } from '../bronze/templ/html/index.mjs';
+import { script, } from '../bronze/templ/html/script/index.mjs';
 
 const sccs=new Success();
 
@@ -9,20 +10,26 @@ const handle=await main(()=>{
     styles: [
       sccs.html,
     ],
+    scripts: [
+      script.wisdom,
+    ],
   });
 console.log( (html`<html>
   <head>${html_head}</head>
   <body>
-    <main>
+    <main id='display'>
     </main>
 
-    <template id='template-leaflets'>
+    <template id='template-top'>
       <header>
         <h1>Leaflets</h1>
       </header>
       <section>
       </section>
     </template>
+    <script> main:{
+      wisdom.write('template-top', 'display');
+    }</script>
   </body>
 </html>`).toString() );
 }, import.meta.filename);
