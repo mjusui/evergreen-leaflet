d30679bcc37f4ea65db57b640a5d2fa4241549ee
mjusui <mjusui@gmail.com>
2026-08-19 17:43:44 +0000

    render

--- src/side_panel.js
+++ src/side_panel.js
@@ -88,8 +88,71 @@ wisdom: {
   wisdom.text=(   ...args)=> wisdom.render('text',    ...args);
   wisdom.html=(   ...args)=> wisdom.render('html',    ...args);
 }
-  const page={};
+const Starray=class Starray {
+  constructor(key){
+    const items=null;
+    const param={ key, items, };
+    this.param=param;
+  }
+  list(){
+    const { key, items, }=this.param;
+    if(!items){
+      const json=localStorage.getItem(key);
+      this.items=JSON.parse(json);
+    }
+    return this.items;
+  }
+  push(...args){
+    const { items, }=this.param;
+    const ret=items.push(...args);
+    this.save();
+    return ret;
+  }
+  filter(...args){
+    const { items, }=this.param;
+    this.items=items.filter(...args);
+    this.save();
+    return this.items;
+  }
+  save(){
+    const { key, items, }=this;
+    const json=JSON.stringify(items);
+    localStorage.setItem(key, json);
+  }
+}
 page: {
+  const page={};
+
+  const store={};
+  store.guide=new Starray('store-guides');
+
+  const loadGuides=()=>{
+    ([ ...document.getElementsByClassName('load-item-guides'), ]).forEach(elem =>{
+      console.log('load-item-guides:', elem);
+      const items=store.guide.list();
+
+      if(items.length < 1){
+        wisdom.text(elem.id, 'ガイドがありません');
+        return;
+      }
+      items.forEach(item =>{
+        const { title, desc, }=item;
+        wisdom.append(elem.id, 'template-item-guide', [ title, desc, ]);
+      });
+    });
+  };
+
+  page.open=(suffix, target='display')=>{
+    wisdom.write(target, 'template-' + suffix);
+    loadGuides();
+  };
+  page.open('top');
+
+  page.clear=(target='display')=>{
+    wisdom.clear(target);
+  };
+
+
   const body=document.getElementsByTagName('body')[0];
   body.addEventListener('submit', ev =>{
     ev.preventDefault();
@@ -100,36 +163,17 @@ page: {
     if(target.classList.contains('modal') ){
       page.clear('modal');
     }
-    if(target.classList.contains('open-modal-add-guide') ){
-      page.open('modal-add-guide', 'modal');
+    if(target.classList.contains('open-modal-add-item-guide') ){
+      page.open('modal-add-item-guide', 'modal');
+    }
+    if(target.classList.contains('add-item-guide') ){
+      const form=target.parentNode;
+      const title=form.title.value;
+      const desc=form.desc.value || '';
+      store.guide.push({ title, desc, });
+      loadGuides();
     }
   });
 
-  page.open=(suffix, target='display')=>{
-    wisdom.write(target, 'template-' + suffix);
-
-    let guides=null;
-    ([ ...document.getElementsByClassName('load-item-guides'), ]).forEach(elem =>{
-      console.log('load-item-guides:', elem);
-
-      if(!guides){
-        const json=localStorage.getItem('json-guides');
-        guides=JSON.parse(json) || [];
-      }
-      if(guides.length < 1){
-        wisdom.text(elem.id, 'ガイドがありません');
-        return;
-      }
-      guides.forEach(guide =>{
-        const { title, desc, }=guide;
-        wisdom.append(elem.id, 'template-item-guide', [ title, desc, ]);
-      });
-    });
-  };
-  page.open('top');
-
-  page.clear=(target='display')=>{
-    wisdom.clear(target);
-  };
 
 }
