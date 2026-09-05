c3f69aa270c5f123dfb0bcd209f8bb828002fde6
mjusui <mjusui@gmail.com>
2026-09-05 10:08:29 +0000

    render

--- render/side_panel.html.mjs
+++ render/side_panel.html.mjs
@@ -242,9 +242,11 @@ console.log( (html`<!DOCTYOE html>
       </div>
     </template>
     <template id='template-item-run-input'>
-      <label class='merge-top' data-render1='textContent' data-render2='for'></label> 
-      <textarea calss='merge-bottom' style='--cols: 2'
-        data-render1='name' data-render2='id' cols='2'></textarea>
+      <div class='col'>
+        <label class='merge-top' data-render1='textContent' data-render2='for'></label> 
+        <textarea class='merge-bottom' style='--cols: 2'
+          data-render1='name' data-render2='id' cols='2'></textarea>
+      </div>
     </template>
 
     <iframe src='sandbox.html'></iframe>
