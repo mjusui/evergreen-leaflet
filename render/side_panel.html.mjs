9f7ffcfb20ea1a6fa2c94428ac8435cac540b369
mjusui <mjusui@gmail.com>
2026-09-06 04:42:25 +0000

    render

--- render/side_panel.html.mjs
+++ render/side_panel.html.mjs
@@ -40,6 +40,9 @@ const scss=new Success()
   .classify('app-theme', 'border-top-style', 'border-top-width: 1px', 'border-top-color: rgba(31, 181, 115, 1.0)',
     `background: linear-gradient(135deg, rgba(20, 130, 85, 1.0) 0%,
       rgba(31, 181, 115, 1.0) 45%, rgba(55, 200, 150, 1.0) 70%, rgba(45, 160, 180, 1.0) 100%)` )
+  .classify('app-theme', 'content: ""', 'height: 1px', 
+    `background: linear-gradient(135deg, rgba(20, 130, 85, 1.0) 0%,
+      rgba(31, 181, 115, 1.0) 45%, rgba(55, 200, 150, 1.0) 70%, rgba(45, 160, 180, 1.0) 100%)` )
   .themify('.col', 'flex-col')
   .themify('.row', 'flex-row')
   .themify('.block-col', '.col', 'block')
@@ -64,7 +67,7 @@ const scss=new Success()
   .themify('.modal', 'modal-hidden')
   .themify('.modal:not(:empty)', 'modal-display', 'flex-col')
   .themify(html`.modal:not(:empty) > *`, 'center', 'middle')
-  .themify('.app-theme', 'app-theme')
+  .themify('body::before', 'app-theme')
 const html_css=scss.html;
 
 const handle=await main(()=>{
