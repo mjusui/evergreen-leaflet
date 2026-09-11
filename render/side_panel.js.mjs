6f615fd0e2ef964d5fb9f88f62a597890a45915d
mjusui <mjusui@gmail.com>
2026-09-11 04:11:04 +0000

    render

--- render/side_panel.js.mjs
+++ render/side_panel.js.mjs
@@ -255,10 +255,6 @@ console.log('vals:', vals);
     const { guideid, }=target.dataset;
     const { name, }=target;
 
-    if(vals){
-      target.value=vals.text;
-    }
-
     const run=Starray.getInst('store-run-' + guideid);
     run.flatMap(item =>{
       const { inputs, }=item;
@@ -269,6 +265,9 @@ console.log('vals:', vals);
       }
       return item;
     }); 
+    if(vals){
+      target.value=vals.text;
+    }
     loadRunOutputs();
   };
 
--- src/side_panel.js
+++ src/side_panel.js
@@ -490,10 +490,6 @@ console.log('vals:', vals);
     const { guideid, }=target.dataset;
     const { name, }=target;
 
-    if(vals){
-      target.value=vals.text;
-    }
-
     const run=Starray.getInst('store-run-' + guideid);
     run.flatMap(item =>{
       const { inputs, }=item;
@@ -504,6 +500,9 @@ console.log('vals:', vals);
       }
       return item;
     }); 
+    if(vals){
+      target.value=vals.text;
+    }
     loadRunOutputs();
   };
 
